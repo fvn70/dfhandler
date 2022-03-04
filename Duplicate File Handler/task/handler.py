0b6f23409e54054872d932452378648498b10b70
fvn70 <fvn_reg@mail.ru>
2022-03-04 22:24:20 +0700

    Stage 2/4: How much does it weigh

--- Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/task/handler.py
@@ -1,11 +1,34 @@
 import os
 import sys
+from os.path import splitext, getsize
 
 args = sys.argv
+rev = False
+
 if len(args) < 2:
     print("Directory is not specified")
 else:
+    ext = input("Enter file format:\n")
+    print("Size sorting options:\n1. Descending\n2. Ascending\n")
+
+    while True:
+        n = input("Enter a sorting option:\n")
+        if n in ("1", "2"):
+            rev = n == "1"
+            break
+        else:
+            print("Wrong option")
+
     dic = {}
     for root, dirs, files in os.walk(args[1]):
         for name in files:
-            print(os.path.join(root, name))
+            ex = splitext(name)[1]
+            if ext and ex != '.' + ext:
+                continue
+            fn = os.path.join(root, name)
+            size = getsize(fn)
+            dic.setdefault(size, []).append(fn)
+
+    for size in sorted(dic, reverse=rev):
+        print(f"\n{size} bytes")
+        print(*dic[size], sep='\n')
