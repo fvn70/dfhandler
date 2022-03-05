b841f55d8fb07e0cb1fd94ada58897b3d9c29cde
fvn70 <fvn_reg@mail.ru>
2022-03-05 11:18:35 +0700

    Stage 3/4: What's that hash about

--- Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/task/handler.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 import sys
 from os.path import splitext, getsize
@@ -5,19 +6,22 @@ from os.path import splitext, getsize
 args = sys.argv
 rev = False
 
+def ask(q, yes, no):
+    while True:
+        cmd = input(f"\n{q}\n").lower()
+        if cmd not in (yes, no):
+            print("Wrong option")
+        else:
+            return cmd == yes
+
+
 if len(args) < 2:
     print("Directory is not specified")
 else:
     ext = input("Enter file format:\n")
     print("Size sorting options:\n1. Descending\n2. Ascending\n")
 
-    while True:
-        n = input("Enter a sorting option:\n")
-        if n in ("1", "2"):
-            rev = n == "1"
-            break
-        else:
-            print("Wrong option")
+    rev = ask("Enter a sorting option:\n", '1', '2')
 
     dic = {}
     for root, dirs, files in os.walk(args[1]):
@@ -32,3 +36,27 @@ else:
     for size in sorted(dic, reverse=rev):
         print(f"\n{size} bytes")
         print(*dic[size], sep='\n')
+
+    if ask("Check for duplicates?", 'yes', 'no'):
+        h_dic = {}
+        for size in dic:
+            for fn in dic[size]:
+                with open(fn, 'rb') as file:
+                    data = file.read()
+                    h = hashlib.md5(data)
+                    h = h.hexdigest()
+                    h_dic.setdefault(size, {}).setdefault(h, []).append(fn)
+
+        h_dic = {size: {h: [fn for fn in h_dic[size][h]] for h in h_dic[size] if len(h_dic[size][h]) > 1}
+                 for size in h_dic}
+
+        n = 0
+        for size in sorted(h_dic, reverse=rev):
+            if len(h_dic[size]) > 0:
+                print(f"\n{size} bytes")
+                for h in h_dic[size]:
+                    print(f"Hash: {h}")
+                    for fn in h_dic[size][h]:
+                        n += 1
+                        print(f"{n}. {fn}")
+
